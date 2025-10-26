cfa2bcd05d49381ec945a6e050787be96a812f85
anuragg0720 <patwardhan.an@northeastern.edu>
2025-10-25 20:08:58 -0400

    UniversityModel-Skeleton

---
+++ UniversityModel-Skeleton/src/model/student/proffrating.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.student;
+
+/**
+ *
+ * @author YAGNIK
+ */
+public class proffrating {
+    
+    private String studentuname;
+    private String professoruname;
+    private int rating;
+    private String subjectcode;
+    private String professorname;
+
+    public String getStudentuname() {
+        return studentuname;
+    }
+
+    public void setStudentuname(String studentuname) {
+        this.studentuname = studentuname;
+    }
+
+    public String getProfessoruname() {
+        return professoruname;
+    }
+
+    public void setProfessoruname(String professoruname) {
+        this.professoruname = professoruname;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getSubjectcode() {
+        return subjectcode;
+    }
+
+    public void setSubjectcode(String subjectcode) {
+        this.subjectcode = subjectcode;
+    }
+
+    public String getProfessorname() {
+        return professorname;
+    }
+
+    public void setProfessorname(String professorname) {
+        this.professorname = professorname;
+    }
+
+    
+}
